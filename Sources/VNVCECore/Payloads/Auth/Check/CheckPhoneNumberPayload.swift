f3cd645357bba06fdc693322aa8b6d09b43a5d99
Kerem Cesme <keremcesme@gmail.com>
2022-11-27 21:34:38 +0300

    asdf

--- Sources/VNVCECore/Payloads/Auth/Check/CheckPhoneNumberPayload.swift
+++ Sources/VNVCECore/Payloads/Auth/Check/CheckPhoneNumberPayload.swift
@@ -1,12 +1,12 @@
 
 import Foundation
 
-struct CheckPhoneNumberPayload {
-    struct V1: Codable {
+public struct CheckPhoneNumberPayload {
+    public struct V1: Codable {
         let clientID: String
         let phoneNumber: String
         
-        enum CodingKeys: String, CodingKey {
+        public enum CodingKeys: String, CodingKey {
             case clientID = "client_id"
             case phoneNumber = "phone_number"
         }
--- Sources/VNVCECore/Payloads/Auth/Check/CheckUsernamePayload.swift
+++ Sources/VNVCECore/Payloads/Auth/Check/CheckUsernamePayload.swift
@@ -1,12 +1,12 @@
 
 import Foundation
 
-struct CheckUsernamePayload {
-    struct V1: Codable {
+public struct CheckUsernamePayload {
+    public struct V1: Codable {
         let clientID: String
         let username: String
         
-        enum CodingKeys: String, CodingKey {
+        public enum CodingKeys: String, CodingKey {
             case clientID = "client_id"
             case username = "phone_number"
         }
