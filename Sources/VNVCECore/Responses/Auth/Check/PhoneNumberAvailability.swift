91197af662827121a29772b47d380a3c0310f3e8
Kerem Cesme <keremcesme@gmail.com>
2022-11-27 21:52:24 +0300

    asdfass

---
+++ Sources/VNVCECore/Responses/Auth/Check/PhoneNumberAvailability.swift
@@ -0,0 +1,27 @@
+
+import Foundation
+
+public struct PhoneNumberAvailability {
+    public enum V1: Codable {
+        case exist
+        case notUsed
+        case otpExpected
+        case otpExpectedBySameUser
+        case blacklisted
+        
+        public var description: String {
+            switch self {
+            case .exist:
+                return "The phone number is exist."
+            case .notUsed:
+                return "The phone number is not used."
+            case .otpExpected:
+                return "Waiting for OTP verification for phone number."
+            case .otpExpectedBySameUser:
+                return "OTP is expected by same user."
+            case .blacklisted:
+                return "The phone number is blacklisted."
+            }
+        }
+    }
+}
---
+++ Sources/VNVCECore/Responses/Auth/Check/UsernameAvailability.swift
@@ -0,0 +1,28 @@
+
+import Foundation
+
+public struct UsernameAvailability {
+    public enum V1 {
+        case used
+        case notUsed
+        case reservedBySameUser
+        case reserved
+        case blacklisted
+        
+        public var description: String {
+            switch self {
+            case .used:
+                return "Username is used."
+            case .notUsed:
+                return "Username is not used."
+            case .reservedBySameUser:
+                return "The username is reserved by the same user."
+            case .reserved:
+                return "Username is reserved."
+            case .blacklisted:
+                return "The username is blacklisted"
+            }
+        }
+    }
+}
+
