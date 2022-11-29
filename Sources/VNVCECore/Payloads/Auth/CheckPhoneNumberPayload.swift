08e4fc4f59bb472343fed4f377113df613e756b4
Kerem Cesme <keremcesme@gmail.com>
2022-11-29 16:48:00 +0300

    asdfas

--- Sources/VNVCECore/Payloads/Auth/CheckPhoneNumberPayload.swift
+++ Sources/VNVCECore/Payloads/Auth/CheckPhoneNumberPayload.swift
@@ -1,10 +1,15 @@
 
 import Foundation
 
-public struct CheckPhoneNumberPayload {
+public final class CheckPhoneNumberPayload {
     public struct V1: Codable {
         public let clientID: String
         public let phoneNumber: String
+        
+        public init(clientID: String, phoneNumber: String) {
+            self.clientID = clientID
+            self.phoneNumber = phoneNumber
+        }
         
         public enum CodingKeys: String, CodingKey {
             case clientID = "client_id"
