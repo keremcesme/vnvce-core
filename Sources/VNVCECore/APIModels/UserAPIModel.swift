25518eb91b7ce51adc846e1e5649b87db8d6ddeb
Kerem Cesme <keremcesme@gmail.com>
2022-12-01 20:12:22 +0300

    asdf a

--- Sources/VNVCECore/APIModels/UserAPIModel.swift
+++ Sources/VNVCECore/APIModels/UserAPIModel.swift
@@ -1,26 +1,27 @@
 
 import Foundation
 
-public struct UserAPIModel: Codable {
-    public let id: UUID
-    public let name: String
-    public let username: String
-    
-    public init(id: UUID, name: String, username: String) {
-        self.id = id
-        self.name = name
-        self.username = username
-    }
-}
-
-extension UserAPIModel {
-    public struct Create: Codable {
-        public let name: String
-        public let username: String
+public final class UserModel {
+    public final class V1 {
         
-        public init(name: String, username: String) {
-            self.name = name
-            self.username = username
+        public struct Public {
+            public let id: UUID
+            public let username: String
+            
+            public let displayName: String?
+            public let biography: String?
+        }
+        
+        public struct Private {
+            public let id: UUID
+            public let username: String
+            public let phoneNumber: String
+            
+            public let displayName: String?
+            public let biography: String?
+            
+            public let createdAt: TimeInterval
+            public let updatedAt: TimeInterval
         }
     }
 }
