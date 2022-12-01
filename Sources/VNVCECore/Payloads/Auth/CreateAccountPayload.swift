84d8eccf5d9e742a57849d468f7fa7a28601626d
Kerem Cesme <keremcesme@gmail.com>
2022-12-01 16:16:04 +0300

    asdfa

---
+++ Sources/VNVCECore/Payloads/Auth/CreateAccountPayload.swift
@@ -0,0 +1,25 @@
+
+import Foundation
+
+final class CreateAccountPayload {
+    public struct V1: Codable {
+        public let username: String
+        public let clientID: String
+        public let phoneNumber: String
+        public let code: String
+        
+        public init(username: String, clientID: String, phoneNumber: String, code: String) {
+            self.username = username
+            self.clientID = clientID
+            self.phoneNumber = phoneNumber
+            self.code = code
+        }
+        
+        enum CodingKeys: String, CodingKey {
+            case username = "username"
+            case clientID = "client_id"
+            case phoneNumber = "phone_number"
+            case code
+        }
+    }
+}
