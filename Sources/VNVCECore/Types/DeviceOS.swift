fd804e8c5191d1676b3e1ca91d2997ee29b08571
Kerem Cesme <keremcesme@gmail.com>
2022-12-04 13:50:20 +0300

    asdf a

--- Sources/VNVCECore/Types/DeviceOS.swift
+++ Sources/VNVCECore/Types/DeviceOS.swift
@@ -4,7 +4,5 @@ import Foundation
 public enum DeviceOS: String, Codable {
     case ios, android
     
-#if canImport(Vapor)
     public static let schema =  "device_os"
-#endif
 }
--- Sources/VNVCECore/Types/MediaType.swift
+++ Sources/VNVCECore/Types/MediaType.swift
@@ -4,6 +4,7 @@ import Foundation
 public enum MediaType: String, Codable {
     case image, movie
     
+
     public static let schema =  "media_type"
 }
 
