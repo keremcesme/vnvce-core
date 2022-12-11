11dd6b5977f8d4282e6588203c572a83e2cd6886
Kerem Cesme <keremcesme@gmail.com>
2022-12-11 21:59:27 +0300

    asdfasdfasdfasdfaasdfasfasdfasasdfa

--- Sources/VNVCECore/Payloads/Auth/AuthorizePayload.swift
+++ Sources/VNVCECore/Payloads/Auth/AuthorizePayload.swift
@@ -5,10 +5,12 @@ public struct AuthorizePayload {
     public struct V1: Codable {
         public let codeChallenge: String
         public let clientID: String
+        public let userID: String
         
         enum CodingKeys: String, CodingKey {
             case codeChallenge = "code_challenge"
             case clientID = "client_id"
+            case userID = "user_id"
         }
     }
 }
