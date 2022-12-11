8c590c1cd569b256f3b405ebcc91a184aef5f7d4
Kerem Cesme <keremcesme@gmail.com>
2022-12-11 22:08:38 +0300

    asdfasdfasdfasdfaasdfasfasdfasasdfaasdfa

--- Sources/VNVCECore/Responses/Auth/AuthorizeResponse.swift
+++ Sources/VNVCECore/Responses/Auth/AuthorizeResponse.swift
@@ -4,9 +4,11 @@ import Foundation
 public final class AuthorizeResponse {
     public struct V1: Codable {
         public let authCode: String
-        init(_ authCode: String) {
+        
+        public init(_ authCode: String) {
             self.authCode = authCode
         }
+        
         enum CodingKeys: String, CodingKey {
             case authCode = "auth_code"
         }
