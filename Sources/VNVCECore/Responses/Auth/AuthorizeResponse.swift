517c86668f663aa30d822addc27ceaeecdc1f68f
Kerem Cesme <keremcesme@gmail.com>
2022-12-13 22:27:50 +0300

    a

--- Sources/VNVCECore/Responses/Auth/AuthorizeResponse.swift
+++ Sources/VNVCECore/Responses/Auth/AuthorizeResponse.swift
@@ -4,13 +4,16 @@ import Foundation
 public final class AuthorizeResponse {
     public struct V1: Codable {
         public let authCode: String
+        public let authID: String
         
-        public init(_ authCode: String) {
+        public init(_ authCode: String, _ authID: String) {
             self.authCode = authCode
+            self.authID = authID
         }
         
         public enum CodingKeys: String, CodingKey {
             case authCode = "auth_code"
+            case authID = "auth_id"
         }
     }
 }
--- Sources/VNVCECore/Responses/Auth/TokenResponse.swift
+++ Sources/VNVCECore/Responses/Auth/TokenResponse.swift
@@ -5,18 +5,15 @@ public final class TokenResponse {
     public struct V1: Codable {
         public let accessToken: String
         public let refreshToken: String
-        public let authID: String
         
-        public init(_ accessToken: String, _ refreshToken: String, _ authID: String) {
+        public init(_ accessToken: String, _ refreshToken: String) {
             self.accessToken = accessToken
             self.refreshToken = refreshToken
-            self.authID = authID
         }
         
         public enum CodingKeys: String, CodingKey {
             case accessToken = "access_token"
             case refreshToken = "refresh_token"
-            case authID = "auth_id"
         }
     }
 }
