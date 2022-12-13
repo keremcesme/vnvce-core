933c9ecccca7fda5ff7e703a4add45af70f9b6a9
Kerem Cesme <keremcesme@gmail.com>
2022-12-13 20:47:13 +0300

    asdf

--- Sources/VNVCECore/Payloads/Auth/AuthorizeParams.swift
+++ Sources/VNVCECore/Payloads/Auth/AuthorizeParams.swift
@@ -1,16 +1,12 @@
 
 import Foundation
 
-public struct AuthorizePayload {
+public struct AuthorizeParams {
     public struct V1: Codable {
         public let codeChallenge: String
-        public let clientID: String
-        public let userID: String
         
         enum CodingKeys: String, CodingKey {
             case codeChallenge = "code_challenge"
-            case clientID = "client_id"
-            case userID = "user_id"
         }
     }
 }
