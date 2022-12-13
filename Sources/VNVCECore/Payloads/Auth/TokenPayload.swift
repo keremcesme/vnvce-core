1f4d6075a648e5dd588aad0c6b9f4dc8639a44fe
Kerem Cesme <keremcesme@gmail.com>
2022-12-13 21:50:51 +0300

    asdfa

--- Sources/VNVCECore/Payloads/Auth/TokenPayload.swift
+++ Sources/VNVCECore/Payloads/Auth/TokenPayload.swift
@@ -5,14 +5,10 @@ public final class TokenPayload {
     public struct V1: Codable {
         public let authCode: String
         public let codeVerifier: String
-        public let clientID: String
-        public let userID: String
         
         public enum CodingKeys: String, CodingKey {
             case authCode = "auth_code"
             case codeVerifier = "code_verifier"
-            case clientID = "client_id"
-            case userID = "user_id"
         }
     }
 }
