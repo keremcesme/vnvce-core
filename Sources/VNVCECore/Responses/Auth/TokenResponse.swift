7c5a5f204ff791c6144565ebda9946ada3682a69
Kerem Cesme <keremcesme@gmail.com>
2022-12-13 22:25:02 +0300

    asdfa

--- Sources/VNVCECore/Responses/Auth/TokenResponse.swift
+++ Sources/VNVCECore/Responses/Auth/TokenResponse.swift
@@ -5,15 +5,18 @@ public final class TokenResponse {
     public struct V1: Codable {
         public let accessToken: String
         public let refreshToken: String
+        public let authID: String
         
-        public init(_ accessToken: String, _ refreshToken: String) {
+        public init(_ accessToken: String, _ refreshToken: String, _ authID: String) {
             self.accessToken = accessToken
             self.refreshToken = refreshToken
+            self.authID = authID
         }
         
         public enum CodingKeys: String, CodingKey {
             case accessToken = "access_token"
             case refreshToken = "refresh_token"
+            case authID = "auth_id"
         }
     }
 }
