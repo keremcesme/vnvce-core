f2b21f942778a00e58c895d6f98f0624af20fc40
Kerem Cesme <keremcesme@gmail.com>
2022-12-22 01:16:50 +0300

    aasdfasdfa

--- Sources/VNVCECore/Payloads/Auth/VerifyOTPAndLoginParams.swift
+++ Sources/VNVCECore/Payloads/Auth/VerifyOTPAndLoginParams.swift
@@ -3,13 +3,12 @@ import Foundation
 
 public final class VerifyOTPAndLoginParams {
     public struct V1: Codable {
-        public struct V1: Codable {
-            public let phoneNumber: String
-            public let code: String
-            enum CodingKeys: String, CodingKey {
-                case phoneNumber = "phone_number"
-                case code
-            }
+        public let phoneNumber: String
+        public let code: String
+        enum CodingKeys: String, CodingKey {
+            case phoneNumber = "phone_number"
+            case code
         }
+        
     }
 }
