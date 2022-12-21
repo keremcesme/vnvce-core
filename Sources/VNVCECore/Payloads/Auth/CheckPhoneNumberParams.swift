77ca41c27366abe29f8a5b9a75adac15bf3a834f
Kerem Cesme <keremcesme@gmail.com>
2022-12-21 03:02:36 +0300

    asfasfafa

---
+++ Sources/VNVCECore/Payloads/Auth/CheckPhoneNumberParams.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+public final class CheckPhoneNumberParams {
+    public struct V1: Codable {
+        public let phoneNumber: String
+        public init(phoneNumber: String) {
+            self.phoneNumber = phoneNumber
+        }
+        public enum CodingKeys: String, CodingKey {
+            case phoneNumber = "phone_number"
+        }
+    }
+}
