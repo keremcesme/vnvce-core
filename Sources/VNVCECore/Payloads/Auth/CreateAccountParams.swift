a8ba3ad58b6d0ed270f009abc304c2a0ae4cf3ee
Kerem Cesme <keremcesme@gmail.com>
2022-12-21 23:23:01 +0300

    asdassdasd

--- Sources/VNVCECore/Payloads/Auth/CreateAccountParams.swift
+++ Sources/VNVCECore/Payloads/Auth/CreateAccountParams.swift
@@ -6,10 +6,12 @@ public final class CreateAccountParams {
         public let username: String
         public let phoneNumber: String
         public let code: String
+        public let codeChallenge: String
         enum CodingKeys: String, CodingKey {
             case username
             case phoneNumber = "phone_number"
             case code
+            case codeChallenge = "code_challenge"
         }
     }
 }
