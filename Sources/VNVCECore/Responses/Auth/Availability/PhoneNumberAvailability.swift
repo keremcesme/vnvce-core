2f1afa8c868d6710de5aa0de0fcaba1ccf4a621c
Kerem Cesme <keremcesme@gmail.com>
2022-12-21 16:39:14 +0300

    asdfasdfa

--- Sources/VNVCECore/Responses/Auth/Availability/PhoneNumberAvailability.swift
+++ Sources/VNVCECore/Responses/Auth/Availability/PhoneNumberAvailability.swift
@@ -3,39 +3,64 @@ import Foundation
 
 public struct PhoneNumberAvailability {
     
-    public struct V1: Codable {
+//    public struct V1: Codable {
+//
+//        public let otp: OTPResponse.V1?
+//        public let availability: Availability
+//
+//        public init(otp: OTPResponse.V1? = nil, availability: Availability) {
+//            self.otp = otp
+//            self.availability = availability
+//        }
+//
+//        public enum Availability: Codable {
+//            case exist
+//            case notUsed
+//            case otpExpected
+//            case otpExpectedBySameUser
+//            case blacklisted
+//
+//            public var description: String {
+//                switch self {
+//                case .exist:
+//                    return "The phone number is exist."
+//                case .notUsed:
+//                    return "The phone number is not used."
+//                case .otpExpected:
+//                    return "Waiting for OTP verification for phone number."
+//                case .otpExpectedBySameUser:
+//                    return "OTP is expected by same user."
+//                case .blacklisted:
+//                    return "The phone number is blacklisted."
+//                }
+//            }
+//        }
+//    }
+    
+    
+    public enum V1: Codable {
+        case exist
+        case notUsed
+        case otpExpected
+        case otpExpectedBySameUser
+        case blacklisted
         
-        public let otp: OTPResponse.V1?
-        public let availability: Availability
-        
-        public init(otp: OTPResponse.V1? = nil, availability: Availability) {
-            self.otp = otp
-            self.availability = availability
-        }
-        
-        public enum Availability: Codable {
-            case exist
-            case notUsed
-            case otpExpected
-            case otpExpectedBySameUser
-            case blacklisted
-            
-            public var description: String {
-                switch self {
-                case .exist:
-                    return "The phone number is exist."
-                case .notUsed:
-                    return "The phone number is not used."
-                case .otpExpected:
-                    return "Waiting for OTP verification for phone number."
-                case .otpExpectedBySameUser:
-                    return "OTP is expected by same user."
-                case .blacklisted:
-                    return "The phone number is blacklisted."
-                }
+        public var description: String {
+            switch self {
+            case .exist:
+                return "The phone number is exist."
+            case .notUsed:
+                return "The phone number is not used."
+            case .otpExpected:
+                return "Waiting for OTP verification for phone number."
+            case .otpExpectedBySameUser:
+                return "OTP is expected by same user."
+            case .blacklisted:
+                return "The phone number is blacklisted."
             }
         }
     }
+    
     
     
 }
