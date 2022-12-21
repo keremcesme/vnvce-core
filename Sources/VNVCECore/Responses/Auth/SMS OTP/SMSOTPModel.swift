205954899a89c5f24e7035ea2bd5f836855064f2
Kerem Cesme <keremcesme@gmail.com>
2022-12-21 03:47:14 +0300

    asdfasdfa

--- Sources/VNVCECore/Responses/Auth/SMS OTP/SMSOTPModel.swift
+++ Sources/VNVCECore/Responses/Auth/SMS OTP/SMSOTPModel.swift
@@ -4,15 +4,21 @@ import Foundation
 public final class SMSOTPModel {
     
     public struct V1: Codable {
+        let otpID: String
+        let otpToken: String
         let createdAt: TimeInterval
         let expireAt: TimeInterval
         
-        public init(createdAt: TimeInterval, expireAt: TimeInterval) {
+        public init(otpID: String, otpToken: String, createdAt: TimeInterval, expireAt: TimeInterval) {
+            self.otpID = otpID
+            self.otpToken = otpToken
             self.createdAt = createdAt
             self.expireAt = expireAt
         }
         
         public enum CodingKeys: String, CodingKey {
+            case otpID = "otp_id"
+            case otpToken = "otp_token"
             case createdAt = "created_at"
             case expireAt = "expire_at"
         }
