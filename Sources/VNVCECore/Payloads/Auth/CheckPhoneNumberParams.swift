f2d6f0d5f9fa103d0fb6a52823ad6d6888d19362
Kerem Cesme <keremcesme@gmail.com>
2023-01-08 21:56:13 +0300

    asdfasfasdfa

--- Sources/VNVCECore/Payloads/Auth/CheckPhoneNumberParams.swift
+++ Sources/VNVCECore/Payloads/Auth/CheckPhoneNumberParams.swift
@@ -4,20 +4,13 @@ import Foundation
 public final class CheckPhoneNumberParams {
     public struct V1: Codable {
         public let phoneNumber: String
-        public let reason: Reason
         
-        public init(phoneNumber: String, reason: Reason) {
+        public init(phoneNumber: String) {
             self.phoneNumber = phoneNumber
-            self.reason = reason
         }
         public enum CodingKeys: String, CodingKey {
             case phoneNumber = "phone_number"
-            case reason
         }
         
-        public enum Reason: String, Codable {
-            case create
-            case login
-        }
     }
 }
