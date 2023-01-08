3616d1b7ba3ff306765ed740f80aaa21ffef1d3c
Kerem Cesme <keremcesme@gmail.com>
2023-01-08 22:11:05 +0300

    asdfasdfas

--- Sources/VNVCECore/Responses/RequestResponse.swift
+++ Sources/VNVCECore/Responses/RequestResponse.swift
@@ -3,8 +3,8 @@ import Foundation
 
 public final class RequestResponse {
     public struct V1: Codable {
-        let error: Bool
-        let message: String?
+        public let error: Bool
+        public let message: String?
         
         public init(error: Bool, message: String? = nil) {
             self.error = error
