2f1b2525957efd544ed4c112eb62fb42d960a316
Kerem Cesme <keremcesme@gmail.com>
2023-01-08 20:48:14 +0300

    asdfasdfasdf

---
+++ Sources/VNVCECore/Headers/Headers.swift
@@ -0,0 +1,24 @@
+
+import Foundation
+
+public struct Headers {
+    static let acceptVersion = "Accept-version"
+    
+    static let refreshToken = "X-Auth-Refresh-Token"
+    
+    static let accessToken = "X-Auth-Access-Token"
+    
+    static let clientID = "X-Client-ID"
+    
+    static let clientOS = "X-Client-OS"
+    
+    static let accessTokenID = "X-Access-Token-ID"
+    
+    static let refreshTokenID = "X-Refresh-Token-ID"
+    
+    static let authID = "X-Auth-ID"
+    
+    static let userID = "X-User-ID"
+    
+    static let otpID = "X-OTP-ID"
+}
