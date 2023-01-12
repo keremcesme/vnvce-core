8c3082a02da7ee5c2277eaa5f53309e9642d4eaa
Kerem Cesme <keremcesme@gmail.com>
2023-01-12 17:53:57 +0300

    asdfasdfasdfsa

--- Sources/VNVCECore/Payloads/Auth/CreateAccountParams.swift
+++ Sources/VNVCECore/Payloads/Auth/CreateAccountParams.swift
@@ -1,17 +1,38 @@
 
 import Foundation
 
-public final class CreateAccountParams {
+public final class CreateAccountPayload {
     public struct V1: Codable {
         public let username: String
-        public let phoneNumber: String
+        
+        public let phoneNumber: PhoneNumber
+        
         public let code: String
         public let codeChallenge: String
+        
+        public init(
+            username: String,
+            phoneNumber: PhoneNumber,
+            code: String,
+            codeChallenge: String
+        ){
+            self.username = username
+            self.phoneNumber = phoneNumber
+            self.code = code
+            self.codeChallenge = codeChallenge
+        }
+        
         enum CodingKeys: String, CodingKey {
             case username
             case phoneNumber = "phone_number"
             case code
             case codeChallenge = "code_challenge"
+        }
+        
+        public struct PhoneNumber: Codable {
+            public let country: String
+            public let countryCode: String
+            public let nationalNumber: Int
         }
     }
 }
