de4633bb4cf9cf36d1167add0cf8dad557ae3f5a
Kerem Cesme <keremcesme@gmail.com>
2023-01-12 21:17:51 +0300

    asdfasdfasdfadsfas

--- Sources/VNVCECore/Payloads/Auth/PKCE/GenerateTokensPayload.swift
+++ Sources/VNVCECore/Payloads/Auth/PKCE/GenerateTokensPayload.swift
@@ -8,6 +8,11 @@ public final class GenerateTokensPayload {
         public let authCode: String
         public let codeVerifier: String
         
+        public init(authCode: String, codeVerifier: String) {
+            self.authCode = authCode
+            self.codeVerifier = codeVerifier
+        }
+        
         public enum CodingKeys: String, CodingKey {
             case authCode = "auth_code"
             case codeVerifier = "code_verifier"
