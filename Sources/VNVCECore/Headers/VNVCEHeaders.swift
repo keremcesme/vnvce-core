f97ef4b73a3b87f309f6bf49e1cc97df7d2d5a64
Kerem Cesme <keremcesme@gmail.com>
2023-01-12 17:46:52 +0300

    asdfasddfassdf

--- Sources/VNVCECore/Headers/VNVCEHeaders.swift
+++ Sources/VNVCECore/Headers/VNVCEHeaders.swift
@@ -3,12 +3,17 @@ import Foundation
 
 public struct VNVCEHeaders {
     public static let acceptVersion = "X-Accept-version"
+    
     public static let refreshToken = "X-Auth-Refresh-Token"
     public static let accessToken = "X-Auth-Access-Token"
+    public static let otpToken = "X-OTP-Token"
+    
     public static let clientID = "X-Client-ID"
     public static let clientOS = "X-Client-OS"
+    
     public static let accessTokenID = "X-Access-Token-ID"
     public static let refreshTokenID = "X-Refresh-Token-ID"
+    
     public static let authID = "X-Auth-ID"
     public static let userID = "X-User-ID"
     public static let otpID = "X-OTP-ID"
