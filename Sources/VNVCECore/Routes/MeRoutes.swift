818d8b03bcb7f852213f9439e7ff0de720709321
Kerem Cesme <keremcesme@gmail.com>
2023-01-13 20:49:43 +0300

    assad

--- Sources/VNVCECore/Routes/MeRoutes.swift
+++ Sources/VNVCECore/Routes/MeRoutes.swift
@@ -4,12 +4,11 @@ import Foundation
 public final class MeRoutes {
     public struct V1 {
         public static let shared = V1()
-        private static let resource = "resource"
-        private static let me = "me".path
-        
+        private static let me = "me"
         private static let edit = "edit".path
         
         // Edit
-        public let editDisplayName = resource + me + edit + "display-name".path
+        public let editDisplayName = me + edit + "display-name".path
+        public let editBiography = me + edit + "biography".path
     }
 }
