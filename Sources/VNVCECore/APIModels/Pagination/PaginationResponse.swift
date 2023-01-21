b2e408dc6d3c8c47aca5bc8f392d638a2b92c214
Kerem Cesme <keremcesme@gmail.com>
2023-01-21 17:20:17 +0300

    asdfasdfasdf

---
+++ Sources/VNVCECore/APIModels/Pagination/PaginationResponse.swift
@@ -0,0 +1,22 @@
+
+import Foundation
+
+public struct PageMetadata: Codable, Hashable {
+    public var page, per, total: Int
+    
+    public init(page: Int = 0, per: Int = 0, total: Int = 0) {
+        self.page = page
+        self.per = per
+        self.total = total
+    }
+}
+
+public struct PaginationResponse<T: Decodable>: Decodable {
+    public var items: [T]
+    public var metadata: PageMetadata
+    
+    public init(items: [T] = [], metadata: PageMetadata = .init()) {
+        self.items = items
+        self.metadata = metadata
+    }
+}
