0e73b1e51cc9217ae2e7865c5f422661c2d8ce2e
Kerem Cesme <keremcesme@gmail.com>
2023-01-21 23:08:12 +0300

    asdfasdfasdf

--- Sources/VNVCECore/Routes/SearchRoutes.swift
+++ Sources/VNVCECore/Routes/SearchRoutes.swift
@@ -4,7 +4,7 @@ import Foundation
 public final class SearchRoutes {
     public struct V1 {
         public static let shared = V1()
-        private static let search = "path".path
+        private static let search = "search".path
         
         public let searchUser = search + "user".path
     }
