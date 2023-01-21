40651db27b42f71304230eed6c0bffd551b6e614
Kerem Cesme <keremcesme@gmail.com>
2023-01-21 14:01:37 +0300

    asdfasdfa

--- Sources/VNVCECore/APIModels/UserAPIModel.swift
+++ Sources/VNVCECore/APIModels/UserAPIModel.swift
@@ -1,21 +1,22 @@
 
 import Foundation
 
-public final class UserModel {
+public final class User {
     public final class V1 {
         
         public struct Public: Codable {
             public let id: UUID
             public let username: String
-            
             public let displayName: String?
-            public let biography: String?
+            public let profilePictureURL: String?
+            public let dateOfBirthYear: Int
             
-            public init(id: UUID, username: String, displayName: String?, biography: String?) {
+            public init(id: UUID, username: String, displayName: String?, profilePictureURL: String?, dateOfBirthYear: Int) {
                 self.id = id
                 self.username = username
                 self.displayName = displayName
-                self.biography = biography
+                self.profilePictureURL = profilePictureURL
+                self.dateOfBirthYear = dateOfBirthYear
             }
         }
         
@@ -23,21 +24,31 @@ public final class UserModel {
             public let id: UUID
             public let username: String
             public let phoneNumber: String
-            
             public let displayName: String?
-            public let biography: String?
-            
+            public let profilePicture: ProfilePicture?
+            public let dateOfBirthYear: Int
             public let createdAt: TimeInterval
-            public let updatedAt: TimeInterval
             
-            public init(id: UUID, username: String, phoneNumber: String, displayName: String?, biography: String?, createdAt: TimeInterval, updatedAt: TimeInterval) {
+            public init(id: UUID, username: String, phoneNumber: String, displayName: String?, profilePicture: ProfilePicture?, dateOfBirthYear: Int, createdAt: TimeInterval) {
                 self.id = id
                 self.username = username
                 self.phoneNumber = phoneNumber
                 self.displayName = displayName
-                self.biography = biography
+                self.profilePicture = profilePicture
+                self.dateOfBirthYear = dateOfBirthYear
                 self.createdAt = createdAt
-                self.updatedAt = updatedAt
+            }
+            
+            public struct ProfilePicture: Codable {
+                public let id: UUID
+                public let url: String
+                public let name: String
+                
+                public init(id: UUID, url: String, name: String) {
+                    self.id = id
+                    self.url = url
+                    self.name = name
+                }
             }
         }
     }
