ec3737ac618512abbada2d3a3a9cc47ec1670478
Kerem Cesme <keremcesme@gmail.com>
2023-02-15 03:40:17 +0300

    asdfasdf

---
+++ Sources/VNVCECore/Routes/TransactionRoutes.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+
+public final class TransactionRoute {
+    public struct V1 {
+        public static let shared = V1()
+        private static let membership = "membership".path
+        
+        public let transaction = membership + "transaction".path
+    }
+}
