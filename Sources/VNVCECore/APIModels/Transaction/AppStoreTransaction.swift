ff13c2da282f61072dbf566d81b1b053e376af6b
Kerem Cesme <keremcesme@gmail.com>
2023-02-16 19:55:39 +0300

    asdfasdfasdfaf

--- Sources/VNVCECore/APIModels/Transaction/AppStoreTransaction.swift
+++ Sources/VNVCECore/APIModels/Transaction/AppStoreTransaction.swift
@@ -9,8 +9,8 @@ public final class AppStoreTransaction {
         public let appAccountToken: UUID
         public let productID: String
         public let appBundleID: String
-        public let purchaseDate: Double
-        public let originalPurchaseDate: Double
+        public let purchaseDate: TimeInterval
+        public let originalPurchaseDate: TimeInterval
         public let purchasedQuantity: Int
         
         public let productType: AppStoreProductType
@@ -19,18 +19,39 @@ public final class AppStoreTransaction {
         /// - Note: Only for subscriptions.
         public let webOrderLineItemID: String?
         public let subscriptionGroupID: String?
-        public let expirationDate: Double?
+        public let expirationDate: TimeInterval?
         public let isUpgraded: Bool?
         public let offerType: AppStoreOfferType?
         public let offerID: String?
         
         
-        public let revocationDate: Double?
+        public let revocationDate: TimeInterval?
         public let revocationReason: AppStoreRevocationReason?
         
-        public let signedDate: Double
+        public let signedDate: TimeInterval
         
-        public init(id: String, originalID: String, appAccountToken: UUID, productID: String, appBundleID: String, purchaseDate: Double, originalPurchaseDate: Double, purchasedQuantity: Int, productType: AppStoreProductType, ownershipType: AppStoreProductOwnershipType, webOrderLineItemID: String?, subscriptionGroupID: String?, expirationDate: Double?, isUpgraded: Bool?, offerType: AppStoreOfferType?, offerID: String?, revocationDate: Double?, revocationReason: AppStoreRevocationReason?, signedDate: Double) {
+        
+        private init(
+            id: String,
+            originalID: String,
+            appAccountToken: UUID,
+            productID: String,
+            appBundleID: String,
+            purchaseDate: TimeInterval,
+            originalPurchaseDate: TimeInterval,
+            purchasedQuantity: Int,
+            productType: AppStoreProductType,
+            ownershipType: AppStoreProductOwnershipType,
+            webOrderLineItemID: String?,
+            subscriptionGroupID: String?,
+            expirationDate: TimeInterval?,
+            isUpgraded: Bool?,
+            offerType: AppStoreOfferType?,
+            offerID: String?,
+            revocationDate: TimeInterval?,
+            revocationReason: AppStoreRevocationReason?,
+            signedDate: TimeInterval
+        ) {
             self.id = id
             self.originalID = originalID
             self.appAccountToken = appAccountToken
@@ -51,30 +72,50 @@ public final class AppStoreTransaction {
             self.revocationReason = revocationReason
             self.signedDate = signedDate
         }
-        
-        public init(id: String, originalID: String, appAccountToken: UUID, productID: String, appBundleID: String, purchaseDate: Date, originalPurchaseDate: Date, purchasedQuantity: Int, productType: AppStoreProductType, ownershipType: AppStoreProductOwnershipType, webOrderLineItemID: String?, subscriptionGroupID: String?, expirationDate: Date?, isUpgraded: Bool?, offerType: AppStoreOfferType?, offerID: String?, revocationDate: Date?, revocationReason: AppStoreRevocationReason?, signedDate: Date) {
-            self.id = id
-            self.originalID = originalID
-            self.appAccountToken = appAccountToken
-            self.productID = productID
-            self.appBundleID = appBundleID
-            self.purchaseDate = purchaseDate.timeIntervalSince1970
-            self.originalPurchaseDate = originalPurchaseDate.timeIntervalSince1970
-            self.purchasedQuantity = purchasedQuantity
-            self.productType = productType
-            self.ownershipType = ownershipType
-            self.webOrderLineItemID = webOrderLineItemID
-            self.subscriptionGroupID = subscriptionGroupID
-            self.expirationDate = expirationDate?.timeIntervalSince1970
-            self.isUpgraded = isUpgraded
-            self.offerType = offerType
-            self.offerID = offerID
-            self.revocationDate = revocationDate?.timeIntervalSince1970
-            self.revocationReason = revocationReason
-            self.signedDate = signedDate.timeIntervalSince1970
-        }
-        
-        
     }
     
 }
+
+//public init(id: String, originalID: String, appAccountToken: UUID, productID: String, appBundleID: String, purchaseDate: Double, originalPurchaseDate: TimeInterval, purchasedQuantity: Int, productType: AppStoreProductType, ownershipType: AppStoreProductOwnershipType, webOrderLineItemID: String?, subscriptionGroupID: String?, expirationDate: TimeInterval?, isUpgraded: Bool?, offerType: AppStoreOfferType?, offerID: String?, revocationDate: TimeInterval?, revocationReason: AppStoreRevocationReason?, signedDate: TimeInterval) {
+//    self.id = id
+//    self.originalID = originalID
+//    self.appAccountToken = appAccountToken
+//    self.productID = productID
+//    self.appBundleID = appBundleID
+//    self.purchaseDate = purchaseDate
+//    self.originalPurchaseDate = originalPurchaseDate
+//    self.purchasedQuantity = purchasedQuantity
+//    self.productType = productType
+//    self.ownershipType = ownershipType
+//    self.webOrderLineItemID = webOrderLineItemID
+//    self.subscriptionGroupID = subscriptionGroupID
+//    self.expirationDate = expirationDate
+//    self.isUpgraded = isUpgraded
+//    self.offerType = offerType
+//    self.offerID = offerID
+//    self.revocationDate = revocationDate
+//    self.revocationReason = revocationReason
+//    self.signedDate = signedDate
+//}
+//
+//public init(id: String, originalID: String, appAccountToken: UUID, productID: String, appBundleID: String, purchaseDate: Date, originalPurchaseDate: Date, purchasedQuantity: Int, productType: AppStoreProductType, ownershipType: AppStoreProductOwnershipType, webOrderLineItemID: String?, subscriptionGroupID: String?, expirationDate: Date?, isUpgraded: Bool?, offerType: AppStoreOfferType?, offerID: String?, revocationDate: Date?, revocationReason: AppStoreRevocationReason?, signedDate: Date) {
+//    self.id = id
+//    self.originalID = originalID
+//    self.appAccountToken = appAccountToken
+//    self.productID = productID
+//    self.appBundleID = appBundleID
+//    self.purchaseDate = purchaseDate.timeIntervalSince1970
+//    self.originalPurchaseDate = originalPurchaseDate.timeIntervalSince1970
+//    self.purchasedQuantity = purchasedQuantity
+//    self.productType = productType
+//    self.ownershipType = ownershipType
+//    self.webOrderLineItemID = webOrderLineItemID
+//    self.subscriptionGroupID = subscriptionGroupID
+//    self.expirationDate = expirationDate?.timeIntervalSince1970
+//    self.isUpgraded = isUpgraded
+//    self.offerType = offerType
+//    self.offerID = offerID
+//    self.revocationDate = revocationDate?.timeIntervalSince1970
+//    self.revocationReason = revocationReason
+//    self.signedDate = signedDate.timeIntervalSince1970
+//}
