a63c6de94ee69a7212889190ed27fa116abb6220
Kerem Cesme <keremcesme@gmail.com>
2023-02-21 17:02:33 +0300

    asdfasdfasdf

--- Sources/VNVCECore/APIModels/Moment/Moments.swift
+++ Sources/VNVCECore/APIModels/Moment/Moments.swift
@@ -4,7 +4,7 @@ import Foundation
 public final class Moments {
     public struct V1: Codable {
         public let owner: User.V1.Public
-        public let moments: [Moment.V1]
+        public var moments: [Moment.V1]
         
         public init(owner: User.V1.Public, moments: [Moment.V1]) {
             self.owner = owner
