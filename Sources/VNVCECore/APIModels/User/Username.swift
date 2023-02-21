a965faa5d45870d54542ae41675d5cd67ab5f49c
Kerem Cesme <keremcesme@gmail.com>
2023-02-21 18:25:55 +0300

    asdf

--- Sources/VNVCECore/APIModels/User/Username.swift
+++ Sources/VNVCECore/APIModels/User/Username.swift
@@ -5,16 +5,22 @@ public final class Username {
     public struct V1: Codable, Equatable, Hashable {
         public let username: String
         public let modifiedAt: TimeInterval
+        public let testValue: Bool
         
-        public init(username: String,
-                    modifiedAt: TimeInterval) {
+        public init(
+            username: String,
+            modifiedAt: TimeInterval,
+            testValue: Bool = false
+        ) {
             self.username = username
             self.modifiedAt = modifiedAt
+            self.testValue = testValue
         }
         
         enum CodingKeys: String, CodingKey {
             case username
             case modifiedAt = "modified_at"
+            case testValue = "test_value"
         }
     }
 }
