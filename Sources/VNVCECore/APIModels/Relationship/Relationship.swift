29c069862498d0d3a8b5454e6bf0a1109e66ea9a
Kerem Cesme <keremcesme@gmail.com>
2023-02-28 20:20:54 +0300

    sdfg

--- Sources/VNVCECore/APIModels/Relationship/Relationship.swift
+++ Sources/VNVCECore/APIModels/Relationship/Relationship.swift
@@ -11,3 +11,48 @@ public final class Relationship {
         case targetUserBlocked
     }
 }
+
+public extension Relationship.V1 {
+    var message: String {
+        switch self {
+        case .nothing:
+            return "Nothing"
+        case let .friend(friendshipID):
+            return "Friendship ID: \(friendshipID)"
+        case let .friendRequestSubmitted(requestID):
+            return "Request is submitted. RequestID: \(requestID)"
+        case let .friendRequestReceived(requestID):
+            return "Request is received. RequestID: \(requestID)"
+        case .targetUserBlocked:
+            return "Target user blocked."
+        case let .blocked(blockID):
+            return "Blocked: \(blockID)"
+        }
+    }
+    
+    var requestID: UUID? {
+        switch self {
+        case let .friendRequestSubmitted(requestID):
+            return requestID
+        case let .friendRequestReceived(requestID):
+            return requestID
+        default: return nil
+        }
+    }
+    
+    var friendshipID: UUID? {
+        switch self {
+        case let .friend(friendshipID):
+            return friendshipID
+        default: return nil
+        }
+    }
+    
+    var blockID: UUID? {
+        switch self {
+        case let .blocked(blockID):
+            return blockID
+        default: return nil
+        }
+    }
+}
