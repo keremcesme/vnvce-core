92159cf491fdb2a140a7ea70af6cec81c5a058c0
Kerem Cesme <keremcesme@gmail.com>
2023-05-06 18:52:40 +0300

    asdfasdfasdfa

--- Sources/VNVCECore/Routes/MomentRoutes.swift
+++ Sources/VNVCECore/Routes/MomentRoutes.swift
@@ -6,6 +6,7 @@ public final class MomentRoutes {
         public static let shared = V1()
         private static let moment = "moment".path
         
+        public let fetchFriendsMoment = moment + "fetch_friends_moments".path
         public let requestID = moment + "request_id".path
         public let upload = moment + "upload".path
         public let delete = moment + "delete".path
